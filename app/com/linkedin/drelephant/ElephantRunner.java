5dff321d981fca1ed55e39b2308c8b0e18a557b7
n.fraison <n.fraison@criteo.com>
2018-05-31 13:58:07 +0200

    Add garmadon Heuristics

--- app/com/linkedin/drelephant/ElephantRunner.java
+++ app/com/linkedin/drelephant/ElephantRunner.java
@@ -16,6 +16,7 @@
 
 package com.linkedin.drelephant;
 
+import com.criteo.drelephant.heuristics.GarmadonTransferHeuristic;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.linkedin.drelephant.analysis.AnalyticJob;
 import com.linkedin.drelephant.analysis.AnalyticJobGenerator;
@@ -58,6 +59,7 @@ public class ElephantRunner implements Runnable {
   private static final String RETENTION_PERIOD_DAY_KEY = "drelephant.analysis.purge.retention.period";
   private static final String PURGE_INTERVAL_SECOND_KEY = "drelephant.analysis.purge.interval";
   private static final String PURGE_BATCH_SIZE_KEY = "drelephant.analysis.purge.batch.size";
+  private static final String GARMADON_TRANSFER_INTERVAL_SECOND_KEY = "drelephant.analysis.garmadon.transfer.interval";
 
   private AtomicBoolean _running = new AtomicBoolean(true);
   private long lastRun;
@@ -69,6 +71,7 @@ public class ElephantRunner implements Runnable {
   private AnalyticJobGenerator _analyticJobGenerator;
 
   private ScheduledExecutorService _purgeScheduler;
+  private ScheduledExecutorService _garmadonTransferScheduler;
 
   private void loadGeneralConfiguration() {
     Configuration configuration = ElephantContext.instance().getGeneralConf();
@@ -108,6 +111,7 @@ public class ElephantRunner implements Runnable {
           loadGeneralConfiguration();
           loadAnalyticJobGenerator();
           ElephantContext.init();
+          setupGarmadonTransferHeuristic();
 
           // Initialize the metrics registries.
           MetricsController.init();
@@ -187,6 +191,21 @@ public class ElephantRunner implements Runnable {
       logger.info("Purge scheduled to remove results of jobs older than " + retentionPeriodDay + " days every " + purgeIntervalSecond + " seconds");
     }
 
+  }
+
+  private void setupGarmadonTransferHeuristic() {
+
+    Configuration configuration = ElephantContext.instance().getGeneralConf();
+
+    final long garmadonTransfertIntervalSecond = Utils.getNonNegativeLong(configuration,
+            GARMADON_TRANSFER_INTERVAL_SECOND_KEY, 60);
+
+    _garmadonTransferScheduler = new ScheduledThreadPoolExecutor(1);
+
+    _garmadonTransferScheduler.scheduleAtFixedRate(new Runnable() {
+      @Override
+      public void run() {GarmadonTransferHeuristic.transfer();}
+    }, 60, garmadonTransfertIntervalSecond, TimeUnit.SECONDS);
   }
 
   private class ExecutorJob implements Runnable {
---
+++ conf/evolutions/default/5.sql
@@ -0,0 +1,53 @@
+#
+# Copyright 2016 LinkedIn Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+# --- !Ups
+CREATE TABLE garmadon_yarn_app_heuristic_result (
+  id                  INT(11)       NOT NULL AUTO_INCREMENT COMMENT 'The application heuristic result id',
+  yarn_app_result_id  VARCHAR(50)   NOT NULL                COMMENT 'The application id',
+  heuristic_class     VARCHAR(255)  NOT NULL                COMMENT 'Name of the JVM class that implements this heuristic',
+  heuristic_name      VARCHAR(128)  NOT NULL                COMMENT 'The heuristic name',
+  severity            TINYINT(2)    UNSIGNED NOT NULL       COMMENT 'The heuristic severity ranging from 0(LOW) to 4(CRITICAL)',
+  score               MEDIUMINT(9)  UNSIGNED DEFAULT 0      COMMENT 'The heuristic score for the application. score = severity * number_of_tasks(map/reduce) where severity not in [0,1], otherwise score = 0',
+  ready               BIT           NOT NULL DEFAULT 0      COMMENT 'Indicate if it is ready to be inserted in dr-elephant heuristics',
+
+  PRIMARY KEY (id)
+);
+
+create index garmadon_yarn_app_heuristic_result_i1 on garmadon_yarn_app_heuristic_result (yarn_app_result_id, ready);
+
+CREATE TABLE garmadon_yarn_app_heuristic_result_details (
+  yarn_app_heuristic_result_id  INT(11) NOT NULL                  COMMENT 'The application heuristic result id',
+  name                          VARCHAR(128) NOT NULL DEFAULT ''  COMMENT 'The analysis detail entry name/key',
+  value                         VARCHAR(255) NOT NULL DEFAULT ''  COMMENT 'The analysis detail value corresponding to the name',
+  details                       TEXT                              COMMENT 'More information on analysis details. e.g, stacktrace',
+
+  PRIMARY KEY (yarn_app_heuristic_result_id,name),
+  CONSTRAINT garmadon_yarn_app_heuristic_result_details_f1 FOREIGN KEY (yarn_app_heuristic_result_id) REFERENCES garmadon_yarn_app_heuristic_result (id)
+);
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE garmadon_yarn_app_heuristic_result;
+
+DROP TABLE garmadon_yarn_app_heuristic_result_details
+
+SET FOREIGN_KEY_CHECKS=1;
+
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
